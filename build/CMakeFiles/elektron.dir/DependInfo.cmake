f9dae1d42d72da5e7cb2a2d99df0bd19afc05163
elektron <jakub.postepski@gmail.com>
2017-01-23 15:20:24 +0100

    first version

--- build/CMakeFiles/elektron.dir/DependInfo.cmake
+++ build/CMakeFiles/elektron.dir/DependInfo.cmake
@@ -1,22 +1,28 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/postep/Pulpit/elektron_stm/src/communicationbuffer.cpp" "/home/postep/Pulpit/elektron_stm/build/CMakeFiles/elektron.dir/communicationbuffer.cpp.o"
-  "/home/postep/Pulpit/elektron_stm/src/elektron4.cpp" "/home/postep/Pulpit/elektron_stm/build/CMakeFiles/elektron.dir/elektron4.cpp.o"
-  "/home/postep/Pulpit/elektron_stm/src/timer.cpp" "/home/postep/Pulpit/elektron_stm/build/CMakeFiles/elektron.dir/timer.cpp.o"
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/robot/jposteps/elektron_stm/src/yahdlc/C/fcs16.c" "/home/robot/jposteps/elektron_stm/build/CMakeFiles/elektron.dir/yahdlc/C/fcs16.c.o"
+  "/home/robot/jposteps/elektron_stm/src/yahdlc/C/yahdlc.c" "/home/robot/jposteps/elektron_stm/build/CMakeFiles/elektron.dir/yahdlc/C/yahdlc.c.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/robot/jposteps/elektron_stm/src/communicationbuffer.cpp" "/home/robot/jposteps/elektron_stm/build/CMakeFiles/elektron.dir/communicationbuffer.cpp.o"
+  "/home/robot/jposteps/elektron_stm/src/elektron4.cpp" "/home/robot/jposteps/elektron_stm/build/CMakeFiles/elektron.dir/elektron4.cpp.o"
+  "/home/robot/jposteps/elektron_stm/src/timer.cpp" "/home/robot/jposteps/elektron_stm/build/CMakeFiles/elektron.dir/timer.cpp.o"
   )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+SET(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
-# Fortran module output directory.
-set(CMAKE_Fortran_TARGET_MODULE_DIR "")
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- build/CMakeFiles/elektron.dir/cmake_clean.cmake
+++ build/CMakeFiles/elektron.dir/cmake_clean.cmake
@@ -1,12 +1,14 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/elektron.dir/elektron4.cpp.o"
   "CMakeFiles/elektron.dir/communicationbuffer.cpp.o"
   "CMakeFiles/elektron.dir/timer.cpp.o"
+  "CMakeFiles/elektron.dir/yahdlc/C/fcs16.c.o"
+  "CMakeFiles/elektron.dir/yahdlc/C/yahdlc.c.o"
   "elektron.pdb"
   "elektron"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/elektron.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/elektron.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
